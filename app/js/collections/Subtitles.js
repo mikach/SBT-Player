3ad4d70ce840253fc56c039ab1b7cc1080c9113d
JOLO- <bahmatjuk@mail.ru>
2014-09-23 20:39:38 +0300

    Little refactoring: code style only

--- app/js/collections/Subtitles.js
+++ app/js/collections/Subtitles.js
@@ -1,10 +1,12 @@
-'strict mode'
+'use strict';
 
 var Backbone = require('backbone');
+var parser = require('subtitles-parser');
+var fs = require('fs');
 
-var Subtitle = require('./js/models/subtitle')
+var Subtitle = require('./js/models/subtitle');
 
-module.exports = Backbone.Collection.extend({
+var Subtitles = Backbone.Collection.extend({
     model: Subtitle,
     index: 0,
 
@@ -23,7 +25,7 @@ module.exports = Backbone.Collection.extend({
         var currentElement;
 
         while (minIndex <= maxIndex) {
-            currentIndex = (minIndex + maxIndex) / 2 | 0;
+            currentIndex = (minIndex + maxIndex) / 2 || 0;
             currentElement = array[currentIndex];
 
             if (currentElement.containMoment(moment) > 0) {
@@ -36,7 +38,18 @@ module.exports = Backbone.Collection.extend({
                 return array[currentIndex];
             }
         }
-
-        throw Error('No subtitle for this moment');
+    }
+}, {
+    /**
+     * Returns {@code Subtitles} collection from the *.srt file
+     */
+    fromFile: function(path) {
+        var srt = fs.readFileSync(path).toString('utf8');
+        var data = parser.fromSrt(srt, true);
+        var subtitles = new Subtitles();
+        subtitles.add(data);
+        return subtitles;
     }
 });
+
+module.exports = Subtitles;
